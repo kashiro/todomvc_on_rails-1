da85913cdb5dadbe0fea8743a5b8384dfc99f3b1
tomoyukikashiro <ksrtmyk@gmail.com>
2018-05-31 21:55:17 +0900

    test(browser): set default driver

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,4 +6,5 @@ Capybara.register_driver :headless_chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
 end
 Capybara.javascript_driver = :headless_chrome
+Capybara.default_driver = :headless_chrome
 Capybara.raise_server_errors = false
